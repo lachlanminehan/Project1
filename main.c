5ed0e7cf8142271301a994e03e44a8909fd6e1d5
Lachlan Minehan <lachlan.minehan115@gmail.com>
2019-04-11 06:44:42 +0000

    rotation cipher

--- main.c
+++ main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 
-
 int main() {
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
-    printf("found intergers %d %d %d\n", a, b, c);\\\
-}
+    char text [100];
+    char ch;
+    int i;
+    int rotator;
+    
+    printf("Enter text:");
+    gets(text);
+    printf("Enter rotator:");
+    scanf("%d", &rotator);
+    
+    for(i = 0 ; text[i] != '\0'; ++i){
+        ch = text[i];
+    
+        
+        if (ch >= 'A' && ch <= 'Z'){
+            ch = ch + rotator;
+            
+            if (ch > 'Z'){
+                ch = ch - 'Z' + 'A' - 1;
+            }
+            text[i] = ch;
+        }
+    }
+     
+     printf("Encrypted message: %s", text);   
+     
+     return 0;
+    }
+
+
